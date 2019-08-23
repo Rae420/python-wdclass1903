6f96207cef1b9a98f1d0e09652b3f4fd28c4f42a
Rae420 <thormiwa@gmail.com>
2019-08-23 07:01:55 -0400

    --- Auto-Git Commit ---

---
+++ chapter6/global.py
@@ -0,0 +1,13 @@
+# def number():
+#     var1 = 5
+
+# print(var1)
+
+
+var2 = 10
+def number1():
+    global var2
+    var2 += 7
+    print(var2)
+
+number1()
---
+++ chapter7/class.py
@@ -0,0 +1,17 @@
+class Car():
+    name = 'bmw_x6'
+    color = 'red'
+    wheels = 4
+
+
+    def welcome(self):
+        print('Welcome to our new car which is ',self.name)
+
+    def car_detail(self):
+        result = 'The name of the car is '+self.name+' and the color is '+self.color+' with '+self.wheels
+        return result
+
+bmw = Car()
+print(bmw.name)
+print(bmw.color)
+print(bmw.wheels)
