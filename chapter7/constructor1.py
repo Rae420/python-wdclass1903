82444a887b4351b8dd45ca1fecfdb417f2e9471a
Rae420 <thormiwa@gmail.com>
2019-08-23 08:02:15 -0400

    --- Auto-Git Commit ---

---
+++ chapter7/constructor1.py
@@ -0,0 +1,10 @@
+class Student():
+
+    def __init__(self):
+        print('Welcome to my frist constructor')
+
+    def detail(self):
+        print('This is normal method')    
+
+Student1 = Student()
+Student1.detail()
